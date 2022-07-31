f331ba2235c4ce2691715a99cc72f41476f2c240
gorojack <1937047112@qq.com>
2022-07-31 22:25:11 +0800

    Animation : Using FABRIK to adjust hand position

--- Source/Blaster/Character/BlasterCharacter.cpp
+++ Source/Blaster/Character/BlasterCharacter.cpp
@@ -231,3 +231,9 @@ bool ABlasterCharacter::IsAiming()
 {
 	return (CombatComponent && CombatComponent->bAiming);
 }
+
+AWeapon* ABlasterCharacter::GetEquippedWeapon()
+{
+	if (CombatComponent == nullptr)return nullptr;
+	return  CombatComponent->EquippedWeapon;
+}
