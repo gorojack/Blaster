7c03c4daf63895ec5d0ca982554b344ad2382339
gorojack <1937047112@qq.com>
2022-08-03 12:01:53 +0800

    Weapon : Projectile tracer

--- Source/Blaster/Weapon/Projectile.cpp
+++ Source/Blaster/Weapon/Projectile.cpp
@@ -5,10 +5,12 @@
 
 #include "Components/BoxComponent.h"
 #include "GameFramework/ProjectileMovementComponent.h"
+#include "Kismet/GameplayStatics.h"
 
 AProjectile::AProjectile()
 {
 	PrimaryActorTick.bCanEverTick = true;
+	bReplicates = true;
 
 	CollisionBox = CreateDefaultSubobject<UBoxComponent>(TEXT("CollisionBox"));
 	SetRootComponent(CollisionBox);
@@ -26,6 +28,13 @@ AProjectile::AProjectile()
 void AProjectile::BeginPlay()
 {
 	Super::BeginPlay();
+
+	if (Tracer)
+	{
+		TracerComponent = UGameplayStatics::SpawnEmitterAttached(Tracer, CollisionBox, FName(), GetActorLocation(),
+		                                                         GetActorRotation(),
+		                                                         EAttachLocation::KeepWorldPosition);
+	}
 }
 
 void AProjectile::Tick(float DeltaTime)
