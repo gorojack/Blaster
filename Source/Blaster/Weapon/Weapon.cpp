680dd46a728cb85c8d0d8727da4c720958b7e53b
gorojack <1937047112@qq.com>
2022-08-03 20:33:16 +0800

    Weapon : Create basic bullet casings

--- Source/Blaster/Weapon/Weapon.cpp
+++ Source/Blaster/Weapon/Weapon.cpp
@@ -3,11 +3,13 @@
 
 #include "Weapon.h"
 
+#include "Casing.h"
 #include "Blaster/Character/BlasterCharacter.h"
 #include "Components/SphereComponent.h"
 #include "Components/WidgetComponent.h"
 #include "Net/UnrealNetwork.h"
 #include "Components/SkeletalMeshComponent.h"
+#include "Engine/SkeletalMeshSocket.h"
 
 AWeapon::AWeapon()
 {
@@ -115,5 +117,20 @@ void AWeapon::Fire(const FVector& HitTarget)
 	if (FireAnimation)
 	{
 		WeaponMesh->PlayAnimation(FireAnimation, false);
+	}
+	if (CasingClass)
+	{
+		const USkeletalMeshSocket* AmmoEjectSocket = GetWeaponMesh()->GetSocketByName(FName("AmmoEject"));
+		if (AmmoEjectSocket)
+		{
+			FTransform SocketTransform = AmmoEjectSocket->GetSocketTransform(WeaponMesh);
+
+			UWorld* World = GetWorld();
+			if (World)
+			{
+				World->SpawnActor<ACasing>(CasingClass, SocketTransform.GetLocation(),
+				                           SocketTransform.GetRotation().Rotator());
+			}
+		}
 	}
 }
